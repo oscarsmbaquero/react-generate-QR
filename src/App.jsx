4c348c56fa9bb126463a1cda21740fafe95a178f
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-08-23 11:51:53 +0200

    load project

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+import './App.css';
+import QRCode from "qrcode";
+import { useState } from 'react';
+
+function App() {
+
+  const [url, setUrl] = useState('');
+  const [qr, setQr] = useState('');
+
+  console.log(qr);
+  const generateCode =(e)=>{
+    e.preventDefault();
+
+    QRCode.toDataURL(url,{
+      width: 400,
+      margin:2,
+      color:{
+        darkcolor:'#EEEEEEFF'
+      }
+    },(err,url)=>{
+      if(err){
+        return console.log(err);
+      }
+      setQr(url)
+    
+    })
+  }
+  return (
+    <div className="App">
+    <header className='header'>
+      <h3>Convierte tu url en qr</h3>
+      <form onSubmit={generateCode}>
+        <input
+          type='text'
+          placeholder='Ingresa la url'
+          value={url}
+          onChange={(e)=> setUrl(e.target.value)}
+        />
+        <br/>
+        <button type='submit'>Generar Código</button>
+      </form>
+      {qr && <div>
+        <br/>
+            <img src={qr} alt='imagen qr'/>
+        <br/>
+        <a href={qr} download='codeQR.png' style={{color:'white'}}>Descargar</a>
+      </div>}
+    </header>
+    </div>
+  );
+}
+
+export default App;
